3146ff54a9866d16a09a872700091ee7925c8c6c
Moitrish Maity <69980373+MoiMaity@users.noreply.github.com>
2021-02-20 11:12:53 +0530

    Create q1.py

---
+++ 2nd Year Python B.Tech./Modules_Packages/q1.py
@@ -0,0 +1,12 @@
+"""
+@author:  Aradhita Dasgupta      2nd year    CSE     Roll - 20
+
+Import datetime module and print current date and time.
+"""
+
+import datetime
+
+date = datetime.datetime.today()
+
+print("Today's date:", date.date())
+print("Current time:", date.time())
